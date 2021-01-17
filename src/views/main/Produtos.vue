da97424e913a1f06d311ee06e37644b8741a4ce1
Lovelex <lukas489@gmail.com>
2021-01-17 13:06:46 -0300

    update: alterado a regra de colunas

--- src/views/main/Produtos.vue
+++ src/views/main/Produtos.vue
@@ -5,8 +5,8 @@
     <span class="caption">Encontrados: {{ items.length }}</span>
     <v-card class="pa-4 d-flex">
       <v-row>
-        <v-col class="d-flex d-sm-block justify-center" cols="12" sm="6" md="4" v-for="product in items" :key="product.id" >
-          <ProductCard :product="product" class="mx-2" />
+        <v-col class="d-flex d-sm-block justify-center" cols="12" sm="6" md="3" v-for="product in items" :key="product.id" >
+          <ProductCard width="240" :product="product" class="mx-2" />
         </v-col>
       </v-row>
     </v-card>
